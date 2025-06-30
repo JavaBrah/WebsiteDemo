153dfd0911257f86a187235ac29edc1db6f29f3a
Kegan <kegcliffjohnson@gmail.com>
2025-06-30 09:20:43 -0400

    g

--- front_end/my-app/src/App.jsx
+++ front_end/my-app/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -9,8 +9,8 @@ import NavBar from './components/NavBar';
 
 function App() {
   const [count, setCount] = useState(0);
-
-
+  
+ 
   return (
     <>
       <NavBar/>
--- front_end/my-app/src/components/CardBody.jsx
+++ front_end/my-app/src/components/CardBody.jsx
@@ -1,10 +1,9 @@
 export default function CardBody({ desc }) {
-  // If you prefer just a paragraph, swap this <ul> for <p>
-  return (
-    <ul className="card-details">
-      {desc.map((d, idx) => (
+    return (
+        <ul className="card-details">
+            {desc.map((d, idx) => (
         <li key={idx}>Phone {d.phone}</li>
       ))}
     </ul>
   );
-}
+};
