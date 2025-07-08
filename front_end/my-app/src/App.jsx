03c08e95aaeb5aeb1aa189900896cad2eff26e57
Kegan <kegcliffjohnson@email.com>
2025-07-07 21:40:12 -0400

    tailwindcss added

--- front_end/my-app/src/App.jsx
+++ front_end/my-app/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import HomeBanner from './components/HomeBanner';
 import NavBar from './components/NavBar';
 
