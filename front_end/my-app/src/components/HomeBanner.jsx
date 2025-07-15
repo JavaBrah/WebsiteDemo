c70ff429eecde29de30f7b3aba6936984846c765
Kegan <kegcliffjohnson@gmail.com>
2025-07-15 09:41:18 -0400

    rewrite

--- front_end/my-app/src/components/HomeBanner.jsx
+++
@@ -1,6 +0,0 @@
-
-
-function HomeBanner(){
-    return <img src='/imageassets/github-header-image.png'/>;
-}
-export default HomeBanner;
---
+++ front_end/src/router.jsx
@@ -0,0 +1,5 @@
+import { createBrowserRouter } from "react-router";
+
+export default function router() {
+    
+};
